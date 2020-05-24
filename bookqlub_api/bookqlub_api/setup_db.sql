2b5f183da54836b253cd1d6c868ff2a01f9dc8fd
Daniel Fujii <danifujii@gmail.com>
2020-05-24 11:53:16 -0300

    DB schema script

---
+++ bookqlub_api/bookqlub_api/setup_db.sql
@@ -0,0 +1,24 @@
+CREATE TABLE users (
+    id          SERIAL  PRIMARY KEY,
+    full_name   VARCHAR NOT NULL,
+    username    VARCHAR NOT NULL
+);
+
+CREATE TABLE books (
+    id              SERIAL  PRIMARY KEY,
+    title           VARCHAR NOT NULL,
+    author          VARCHAR NOT NULL,
+    release_date    TIMESTAMP WITHOUT TIME ZONE NOT NULL
+);
+
+CREATE TYPE review AS ENUM ('EXCELLENT', 'GREAT', 'GOOD', 'OK');
+
+CREATE TABLE reviews (
+    user_id INTEGER NOT NULL,
+    book_id INTEGER NOT NULL,
+    value   REVIEW  NOT NULL,
+    comment VARCHAR,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (book_id) REFERENCES books (id),
+    PRIMARY KEY(user_id, book_id)
+);
