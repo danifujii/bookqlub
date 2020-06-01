f028731c4f83731c58e8fbc25924d36d763fb0e0
Daniel Fujii <danifujii@gmail.com>
2020-05-31 23:10:23 -0300

    Base of login screen and connection to server

--- bookqlub_app/src/App.js
+++ bookqlub_app/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Login from "./components/Login";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+export const API_URL = process.env.BOOKQLUB_API_URL;
+
+const client = new ApolloClient({
+  uri: API_URL,
+});
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello!</p>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Container maxWidth="md">
+          <Login />
+        </Container>
+      </div>
+    </ApolloProvider>
   );
 }
 
