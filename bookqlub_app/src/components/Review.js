c897b99e097a3c18990233f60ff7af772fa2ca6b
Daniel Fujii <danifujii@gmail.com>
2020-06-12 15:43:43 -0300

    Connect to backend to fetch actual reviews

--- bookqlub_app/src/components/Review.js
+++ bookqlub_app/src/components/Review.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-export const Review = () => {
+export const Review = (props) => {
+  const { review } = props;
+
   return (
     <div className="ReviewContainer">
       <div className="ReviewCover">
         <img
-          src="https://images-na.ssl-images-amazon.com/images/I/91twTG-CQ8L.jpg"
-          alt="Book cover"
+          src={review.book.coverUrl}
+          alt={`${review.book.title} cover`}
           width="100%"
           height="100%"
         />
@@ -14,14 +16,23 @@ export const Review = () => {
 
       <div>
         <div className="ReviewDetailContainer">
-          <p className="ReviewDetailTitle">Little Fires Everywhere</p>
+          <p className="ReviewDetailTitle">{review.book.title}</p>
           <p>
-            <i>Celeste Ng</i>
+            <i>{review.book.author}</i>
           </p>
-          <p className="ReviewDetailValue">GREAT</p>
-          <p className="ReviewDetailDate">14 January</p>
+          <p className="ReviewDetailValue">{review.value}</p>
+          <p className="ReviewDetailDate">{getDate(review.created)}</p>
         </div>
       </div>
     </div>
   );
 };
+
+function getDate(createdDate) {
+  const date = new Date(createdDate);
+  const monthName = date.toLocaleString("default", {
+    month: "long",
+  });
+
+  return `${date.getDate()} ${monthName}`;
+}
--- bookqlub_app/src/components/ReviewsGrid.js
+++ bookqlub_app/src/components/ReviewsGrid.js
@@ -1,21 +1,50 @@
 import React from "react";
-import { Review } from "./Review";
-import { Grid } from "@material-ui/core";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
+import { LinearProgress, Grid } from "@material-ui/core";
 
-export const ReviewGrid = () => {
+import { Review } from "./Review";
+
+const GET_REVIEWS = gql`
+  {
+    reviews {
+      created
+      value
+      book {
+        author
+        title
+        coverUrl
+      }
+    }
+  }
+`;
+
+export const ReviewGrid = (props) => {
+  const { loading, error, data } = useQuery(GET_REVIEWS);
+
+  if (loading) {
+    return (
+      <div className="ReviewLoading">
+        <LinearProgress />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <p className="ErrorMsg">{`There was an error loading your reviews in this year.`}</p>
+    );
+  }
+
   return (
     <div>
       <h2>January</h2>
       <Grid container spacing={2}>
-        <Grid item lg={4} md={6} xs={12}>
-          <Review />
-        </Grid>
-        <Grid item lg={4} md={6} xs={12}>
-          <Review />
-        </Grid>
-        <Grid item lg={4} md={6} xs={12}>
-          <Review />
-        </Grid>
+        {data.reviews.map((review, idx) => (
+          <Grid item lg={4} md={6} xs={12} key={idx}>
+            <Review review={review} />
+          </Grid>
+        ))}
       </Grid>
     </div>
   );
