4a21fb4e3beb1a7b3037cfb9cd8fcfef6543ccb4
Daniel Fujii <danifujii@gmail.com>
2020-06-13 11:20:45 -0300

    Add review modal

--- bookqlub_app/src/components/AddReview.js
+++ bookqlub_app/src/components/AddReview.js
@@ -1,8 +1,19 @@
 import React, { useState } from "react";
+import DateFnsUtils from "@date-io/date-fns";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
 import Fab from "@material-ui/core/Fab";
-import Modal from "@material-ui/core/Modal";
-import Paper from "@material-ui/core/Paper";
+import Input from "@material-ui/core/Input";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
 import AddIcon from "@material-ui/icons/Add";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+
+const REVIEW_VALUES = ["Excellent", "Great", "Good", "Ok"];
 
 export const AddReviewButton = () => {
   const [modalOpen, setModalOpen] = useState(false);
@@ -17,23 +28,81 @@ export const AddReviewButton = () => {
         Add review
       </Fab>
 
-      <AddReviewModal
+      <Dialog
+        onClose={() => setModalOpen(false)}
         open={modalOpen}
-        handleClose={() => setModalOpen(false)}
-      />
+        maxWidth="md"
+      >
+        <AddReviewModalBody />
+      </Dialog>
     </div>
   );
 };
 
-const AddReviewModal = (props) => {
-  const { handleClose, open } = props;
+const AddReviewModalBody = (props) => {
+  const { imageUrl } = props;
   return (
-    <Modal onClose={handleClose} open={open} outline="0">
-      <div>
-        <Paper>
-          <h1>Add review</h1>
-        </Paper>
+    <div className="AddReviewModalContainer">
+      <div className="AddReviewCoverContainer">
+        {imageUrl ? (
+          <img src={imageUrl} alt="Book cover" className="AddReviewCover" />
+        ) : (
+          <p>Book cover</p>
+        )}
       </div>
-    </Modal>
+      <AddReviewModalContent />
+    </div>
+  );
+};
+
+const AddReviewModalContent = (props) => {
+  const [reviewDate, setReviewDate] = useState(new Date());
+
+  return (
+    <div className="AddReviewContentContainer">
+      <h1>Add review</h1>
+
+      <h3 className="AddReviewSubtitle">Title</h3>
+      <Input placeholder="Book title" fullWidth />
+
+      <h3 className="AddReviewSubtitle">Value</h3>
+      <Select fullWidth placeholder="Value">
+        {REVIEW_VALUES.map((value) => (
+          <MenuItem value={value} key={value}>
+            {value}
+          </MenuItem>
+        ))}
+      </Select>
+
+      <h3 className="AddReviewSubtitle">Finish date</h3>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="MM/dd/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          value={reviewDate}
+          onChange={(date) => setReviewDate(date)}
+          KeyboardButtonProps={{
+            "aria-label": "change date",
+          }}
+          style={{ marginTop: 0 }}
+          fullWidth
+        />
+      </MuiPickersUtilsProvider>
+
+      <h3 className="AddReviewSubtitle">Comment</h3>
+      <Input placeholder="Review comments" fullWidth multiline rows={3} />
+
+      <div className="AddReviewButtons">
+        <DialogActions>
+          <Button color="primary">Cancel</Button>
+          <Button variant="contained" color="primary">
+            Add review
+          </Button>
+        </DialogActions>
+      </div>
+    </div>
   );
 };
