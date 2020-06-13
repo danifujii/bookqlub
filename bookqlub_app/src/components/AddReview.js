a555c271aafc8702ecacc89e598c752a47a4335b
Daniel Fujii <danifujii@gmail.com>
2020-06-13 17:29:11 -0300

    Add book title lookup

--- bookqlub_app/src/components/AddReview.js
+++ bookqlub_app/src/components/AddReview.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { gql } from "apollo-boost";
 import DateFnsUtils from "@date-io/date-fns";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
@@ -8,12 +9,25 @@ import Input from "@material-ui/core/Input";
 import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
 import AddIcon from "@material-ui/icons/Add";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 import {
   MuiPickersUtilsProvider,
   KeyboardDatePicker,
 } from "@material-ui/pickers";
+import { CircularProgress, TextField } from "@material-ui/core";
+import { useLazyQuery } from "@apollo/react-hooks";
+import _ from "lodash";
 
 const REVIEW_VALUES = ["Excellent", "Great", "Good", "Ok"];
+
+const GET_BOOKS = gql`
+  query BooksByTitle($title: String!) {
+    booksByTitle(title: $title) {
+      id
+      title
+    }
+  }
+`;
 
 export const AddReviewButton = () => {
   const [modalOpen, setModalOpen] = useState(false);
@@ -50,23 +64,66 @@ const AddReviewModalBody = (props) => {
           <p>Book cover</p>
         )}
       </div>
-      <AddReviewModalContent />
+      <AddReviewModalForm />
     </div>
   );
 };
 
-const AddReviewModalContent = (props) => {
+const AddReviewModalForm = () => {
+  const [title, setTitle] = useState(undefined);
   const [reviewDate, setReviewDate] = useState(new Date());
+
+  const [titleOpen, setTitleOpen] = useState(false);
+
+  const [getBooks, { loading, error, data }] = useLazyQuery(GET_BOOKS);
+
+  const handleUserInput = (event) => {
+    event.persist();
+    queryBooks(event);
+  };
+
+  const queryBooks = _.debounce((event) => {
+    const titleQuery = event.target.value;
+    if (titleQuery) {
+      getBooks({ variables: { title: titleQuery } });
+    }
+  }, 500);
 
   return (
     <div className="AddReviewContentContainer">
       <h1>Add review</h1>
 
       <h3 className="AddReviewSubtitle">Title</h3>
-      <Input placeholder="Book title" fullWidth />
+      <Autocomplete
+        open={titleOpen}
+        fullWidth
+        onOpen={() => setTitleOpen(true)}
+        onClose={() => setTitleOpen(false)}
+        onChange={(_, value) => setTitle(value)}
+        getOptionLabel={(option) => option.title}
+        options={(data && data.booksByTitle) || []}
+        loading={loading}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            placeholder="Book title"
+            fullWidth
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <React.Fragment>
+                  {loading && <CircularProgress color="primary" size={20} />}
+                  {params.InputProps.endAdornment}
+                </React.Fragment>
+              ),
+            }}
+            onChange={handleUserInput}
+          />
+        )}
+      />
 
       <h3 className="AddReviewSubtitle">Value</h3>
-      <Select fullWidth placeholder="Value">
+      <Select fullWidth placeholder="Value" value={REVIEW_VALUES[0]}>
         {REVIEW_VALUES.map((value) => (
           <MenuItem value={value} key={value}>
             {value}
