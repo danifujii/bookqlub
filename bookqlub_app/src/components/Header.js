b62f043efc1349294d1b48dc2ef3789f7d862224
Daniel Fujii <danifujii@gmail.com>
2020-06-14 11:57:36 -0300

    Add logout button to header

--- bookqlub_app/src/components/Header.js
+++ bookqlub_app/src/components/Header.js
@@ -1,9 +1,20 @@
 import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import MeetingRoomRoundedIcon from "@material-ui/icons/MeetingRoomRounded";
+import { Tooltip } from "@material-ui/core";
 
 export const Header = () => {
   return (
     <div className="HeaderContainer">
-      <h1 className="LoginHeader HeaderContent">Bookqlub</h1>
+      <div className="HeaderContent">
+        <h1 className="LoginHeader HeaderTitle">Bookqlub</h1>
+
+        <Tooltip title="Logout">
+          <IconButton className="HeaderButton">
+            <MeetingRoomRoundedIcon className="HeaderButtonIcon" />
+          </IconButton>
+        </Tooltip>
+      </div>
       <div className="HeaderDivider" />
     </div>
   );
