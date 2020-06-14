a47378da09cb29575e099710623549c35cd73b84
Daniel Fujii <danifujii@gmail.com>
2020-06-14 12:25:32 -0300

    Finish up logout

--- bookqlub_app/src/components/Header.js
+++ bookqlub_app/src/components/Header.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import IconButton from "@material-ui/core/IconButton";
 import MeetingRoomRoundedIcon from "@material-ui/icons/MeetingRoomRounded";
 import { Tooltip } from "@material-ui/core";
 
+import { UserContext } from "../App";
+
 export const Header = () => {
+  const { setUsername } = useContext(UserContext);
+
   return (
     <div className="HeaderContainer">
       <div className="HeaderContent">
         <h1 className="LoginHeader HeaderTitle">Bookqlub</h1>
 
         <Tooltip title="Logout">
-          <IconButton className="HeaderButton">
+          <IconButton
+            className="HeaderButton"
+            onClick={() => logout(setUsername)}
+          >
             <MeetingRoomRoundedIcon className="HeaderButtonIcon" />
           </IconButton>
         </Tooltip>
@@ -18,4 +25,10 @@ export const Header = () => {
       <div className="HeaderDivider" />
     </div>
   );
+};
+
+const logout = (setUsername) => {
+  localStorage.removeItem("token");
+  localStorage.removeItem("username");
+  setUsername(undefined);
 };
