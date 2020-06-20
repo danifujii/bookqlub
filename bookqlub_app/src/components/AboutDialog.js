1b7b477ee7674c0323981d1eea7590cb22697c2e
Daniel Fujii <danifujii@gmail.com>
2020-06-20 17:48:11 -0300

    Add About dialog

---
+++ bookqlub_app/src/components/AboutDialog.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { Button } from "@material-ui/core";
+
+export const AboutDialog = (props) => {
+  const { open, onClose } = props;
+
+  return (
+    <div>
+      <Dialog open={open}>
+        <DialogTitle>About Bookqlub</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Bookqlub is a book review site built by Daniel Fujii.
+          </DialogContentText>
+          <DialogContentText>
+            You can find more information about the implementation and its
+            source code in &nbsp;
+            <a
+              href="https://github.com/danifujii/bookqlub"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              the Github repository.
+            </a>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose} color="primary" autoFocus>
+            Understood
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
--- bookqlub_app/src/components/Footer.js
+++ bookqlub_app/src/components/Footer.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { AboutDialog } from "./AboutDialog";
 
 export const Footer = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleLinkClick = (e) => {
+    e.preventDefault();
+    setOpen(true);
+  };
+
   return (
     <div className="FooterContainer">
       <div className="FooterDivider" />
       <p>&copy; 2020, designed and built by Daniel Fujii &middot;</p>
-      <a href="#">
+      <a href="" onClick={handleLinkClick}>
         <p>&nbsp;About</p>
       </a>
+      <AboutDialog open={open} onClose={() => setOpen(false)} />
     </div>
   );
 };
