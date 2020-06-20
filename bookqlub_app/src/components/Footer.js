0dbd680c365b3ba2bdfd0c2eb17ecd28e466f8cd
Daniel Fujii <danifujii@gmail.com>
2020-06-20 17:29:17 -0300

    Footer base

---
+++ bookqlub_app/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="FooterContainer">
+      <div className="FooterDivider" />
+      <p>&copy; 2020, designed and built by Daniel Fujii &middot;</p>
+      <a href="#">
+        <p>&nbsp;About</p>
+      </a>
+    </div>
+  );
+};
--- bookqlub_app/src/pages/Homepage.js
+++ bookqlub_app/src/pages/Homepage.js
@@ -3,13 +3,15 @@ import Container from "@material-ui/core/Container";
 
 import { Header } from "../components/Header";
 import { ReviewsContainer } from "../components/ReviewsContainer";
+import { Footer } from "../components/Footer";
 
 export const Homepage = () => {
   return (
-    <div>
-      <Container maxWidth="lg">
+    <div style={{ height: "100%" }}>
+      <Container maxWidth="lg" className="HomepageContainer">
         <Header />
         <ReviewsContainer />
+        <Footer />
       </Container>
     </div>
   );
