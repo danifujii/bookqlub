de072b34c1d143be27581819bd3891a6389d5fee
Daniel Fujii <danifujii@gmail.com>
2020-06-27 10:58:05 -0300

    Refactor tests to use inmemory DB and increase coverage

--- bookqlub_api/tests/test_schema.py
+++ bookqlub_api/tests/test_schema.py
@@ -1,15 +1,14 @@
 from datetime import datetime
 from typing import Dict
-from unittest import mock
 import unittest
 
-from alchemy_mock.mocking import UnifiedAlchemyMagicMock
 from freezegun import freeze_time
 import bcrypt
 import jwt
+import sqlalchemy as SA
 
 from bookqlub_api import application, utils
-from bookqlub_api.schema import models, utils as schema_utils
+from bookqlub_api.schema import models, queries, utils as schema_utils
 
 
 class BaseTestSchema(unittest.TestCase):
@@ -17,8 +16,10 @@ class BaseTestSchema(unittest.TestCase):
     demo_user_id = -2
 
     def setUp(self):
-        if not hasattr(self, "session"):  # If children test hasn't already defined a session
-            self.session = UnifiedAlchemyMagicMock()
+        engine = SA.create_engine("sqlite:///:memory:")
+        models.Base.metadata.create_all(engine)
+        self.session = SA.orm.sessionmaker(bind=engine)()
+
         self.client = application.create_app(self.session).test_client()
 
         self.og_demo_user_ids = utils.config["app"]["demo_user_ids"]
@@ -60,17 +61,6 @@ class TestUserSchema(BaseTestSchema):
         }
     """
 
-    def setUp(self):
-        self.session = UnifiedAlchemyMagicMock(
-            data=[
-                (
-                    [mock.call.filter(models.User.username == "dan")],
-                    [models.User(full_name="Daniel", username="dan", password="hi")],
-                ),
-            ]
-        )
-        super().setUp()
-
     def test_user_creation(self):
         # Create new user
         variables = {"full_name": "Daniel", "username": "daniel", "pass": "hello"}
@@ -93,7 +83,10 @@ class TestUserSchema(BaseTestSchema):
         self.assertEqual(user.get("username"), variables.get("username"))
 
     def test_user_already_exists(self):
-        variables = {"full_name": "Daniel", "username": "dan", "pass": "hello"}
+        variables = {"full_name": "Daniel", "username": "daniel", "pass": "hello"}
+        _ = self.graphql_request(self.mutation, variables)
+
+        variables = {"full_name": "Gabe", "username": "daniel", "pass": "hello2"}
         errors = self.graphql_request(self.mutation, variables).get("errors")
         self.assertTrue(errors)
         self.assertEqual(errors[0].get("message"), "Username already exists")
@@ -111,16 +104,10 @@ class TestLoginSchema(BaseTestSchema):
     password = "hello"
 
     def setUp(self):
-        password = bcrypt.hashpw(self.password.encode(), bcrypt.gensalt()).decode("utf8")
-        self.session = UnifiedAlchemyMagicMock(
-            data=[
-                (
-                    [mock.call.filter(models.User.username == "dan")],
-                    [models.User(username="dan", full_name="Daniel", password=password)],
-                ),
-            ]
-        )
         super().setUp()
+        password = bcrypt.hashpw(self.password.encode(), bcrypt.gensalt()).decode("utf8")
+        self.session.add(models.User(username="dan", full_name="Daniel", password=password))
+        self.session.commit()
 
     def test_user_login(self):
         variables = {"username": "dan", "pass": self.password}
@@ -163,11 +150,6 @@ class TestReviewSchema(BaseTestSchema):
         }
     """
 
-    def setUp(self):
-        super().setUp()
-        self.session.add(models.User(username="p", full_name="Peter"))
-        self.session.add(models.Book(title="A book", author="Me", release_date=datetime.now()))
-
     def test_review_creation(self):
         # Create new review
         variables = {
@@ -179,14 +161,14 @@ class TestReviewSchema(BaseTestSchema):
         self.graphql_request(self.review_mutation, variables, self.get_headers_with_auth())
 
         # Check review was saved correctly
-        query = "{ reviews(year: 2020) { comment, created } }"
+        query = "{ reviews(year: 2020) { items { comment, created } } }"
         resp_data = self.graphql_request(query, headers=self.get_headers_with_auth()).get(
             "data", {}
         )
-        reviews = resp_data.get("reviews")
+        reviews = resp_data.get("reviews", {}).get("items", [])
         self.assertTrue(reviews)
         self.assertEqual(reviews[0].get("comment"), variables["comment"])
-        self.assertEqual(reviews[0].get("created"), variables["date"])
+        self.assertIn(variables["date"], reviews[0].get("created"))
 
     def test_review_creation_demo_user(self):
         variables = {
@@ -200,18 +182,12 @@ class TestReviewSchema(BaseTestSchema):
         self.assertTrue(errors)
         self.assertIn("Invalid action", errors[0].get("message"))
 
-    def test_reviews_unauthorized(self):
-        query = "{ reviews(year: 2020) { comment } }"
-        errors = self.graphql_request(query).get("errors")
-        self.assertTrue(errors)
-        self.assertEqual(errors[0].get("message"), "Invalid authentication token")
-
     def test_review_years(self):
         years = [2020, 2019, 2018]
         for year in years:
             with freeze_time(f"{year}-01-01"):
                 variables = {
-                    "book_id": 1,
+                    "book_id": year,  # Just random book ID is fine
                     "comment": "Pretty good book",
                     "value": "GOOD",
                 }
@@ -219,4 +195,92 @@ class TestReviewSchema(BaseTestSchema):
 
         query = "{ reviewsYears }"
         data = self.graphql_request(query, headers=self.get_headers_with_auth()).get("data")
-        self.assertListEqual(data.get("reviewsYears", []), years)
+        self.assertListEqual(data.get("reviewsYears", []), sorted(years))
+
+
+class TestReviewListSchema(BaseTestSchema):
+    def setUp(self):
+        super().setUp()
+
+        self.total_pages = 2
+        for n in range(queries.REVIEW_PAGE_SIZE * self.total_pages):
+            self.session.add(
+                models.Review(
+                    user_id=1,
+                    book_id=n,
+                    value="GOOD",
+                    comment="",
+                    created=datetime.strptime("2020-01-01", "%Y-%m-%d"),
+                )
+            )
+        self.session.commit()
+
+    def test_reviews_unauthorized(self):
+        query = "{ reviews(year: 2020) { items { comment } } }"
+        errors = self.graphql_request(query).get("errors")
+        self.assertTrue(errors)
+        self.assertEqual(errors[0].get("message"), "Invalid authentication token")
+
+    def test_reviews(self):
+        book_ids = set()
+        for page in range(self.total_pages):
+            query = "{ reviews(year: 2020, page: %d) { items { bookId } }}" % (page + 1)
+            reviews = (
+                self.graphql_request(query, headers=self.get_headers_with_auth())
+                .get("data", {})
+                .get("reviews", {})
+                .get("items", [])
+            )
+            self.assertTrue(reviews)
+            self.assertEqual(len(reviews), queries.REVIEW_PAGE_SIZE)
+            for review in reviews:
+                book_id = review["bookId"]
+                self.assertNotIn(book_id, book_ids)
+                book_ids.add(book_id)
+
+
+class TestBooksSchema(BaseTestSchema):
+
+    BOOKS_QUERY = '{ booksByTitle(title: "%s") { title } }'
+
+    def setUp(self):
+        super().setUp()
+        self.session.add(models.Book(title="This", author="Author"))
+        self.session.add(models.Book(title="Thi", author="Author"))
+        self.session.add(models.Book(title="The", author="Author"))
+        self.session.commit()
+
+    def testBooksTitleSearch(self):
+        books = (
+            self.graphql_request(self.BOOKS_QUERY % "thi", headers=self.get_headers_with_auth())
+            .get("data", {})
+            .get("booksByTitle", [])
+        )
+        for book_info in books:
+            self.assertIn("thi", book_info["title"].lower())
+
+    def testBooksFilterReviewed(self):
+        user_id = 256
+        book_id = 512
+        self.session.add(models.Book(id=book_id, title="This is", author="Another author"))
+        self.session.add(models.Review(user_id=user_id, book_id=book_id, value="GOOD"))
+
+        books = (
+            self.graphql_request(
+                self.BOOKS_QUERY % "thi", headers=self.get_headers_with_auth(user_id=user_id)
+            )
+            .get("data", {})
+            .get("booksByTitle", [])
+        )
+        self.assertEqual(len(books), 2)
+
+    def testBooksBelowLimit(self):
+        for n in range(queries.SEARCH_LIMIT * 2):
+            self.session.add(models.Book(title=f"The {n} book"))
+
+        books = (
+            self.graphql_request(self.BOOKS_QUERY % "The", headers=self.get_headers_with_auth())
+            .get("data", {})
+            .get("booksByTitle", [])
+        )
+        self.assertEqual(len(books), queries.SEARCH_LIMIT)
