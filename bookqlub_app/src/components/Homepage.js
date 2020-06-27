4a4edf763cd8003a6aca3445c8361774bb049184
Daniel Fujii <danifujii@gmail.com>
2020-06-27 18:34:10 -0300

    Base of navbar impl

--- bookqlub_app/src/components/Homepage.js
+++ bookqlub_app/src/components/Homepage.js
@@ -4,12 +4,14 @@ import Container from "@material-ui/core/Container";
 import { Header } from "./common/Header";
 import { ReviewsContainer } from "./review_grid/ReviewsContainer";
 import { Footer } from "./common/Footer";
+import { Navbar } from "./common/Navbar";
 
 export const Homepage = () => {
   return (
     <div style={{ height: "100%" }}>
       <Container maxWidth="lg" className="HomepageContainer">
         <Header />
+        <Navbar />
         <ReviewsContainer />
         <Footer />
       </Container>
