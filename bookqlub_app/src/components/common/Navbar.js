e5fb0ad6f14c26be512f3f7bbecb6c6d14d388f2
Daniel Fujii <danifujii@gmail.com>
2020-06-28 10:00:36 -0300

    Key for each navbar element

--- bookqlub_app/src/components/common/Navbar.js
+++ bookqlub_app/src/components/common/Navbar.js
@@ -9,7 +9,7 @@ export const Navbar = () => {
   return (
     <div className="NavbarContainer">
       {Object.keys(linkTitles).map((link) => (
-        <NavLink to={link} exact activeClassName="NavLinkActive">
+        <NavLink to={link} exact activeClassName="NavLinkActive" key={link}>
           {linkTitles[link]}
         </NavLink>
       ))}
