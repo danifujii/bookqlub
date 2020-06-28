7af565e7dfba140db8ce7e06c075ae3a851f8ff7
Daniel Fujii <danifujii@gmail.com>
2020-06-28 09:51:15 -0300

    Fix to active status in navbar

--- bookqlub_app/src/components/common/Navbar.js
+++ bookqlub_app/src/components/common/Navbar.js
@@ -9,7 +9,7 @@ export const Navbar = () => {
   return (
     <div className="NavbarContainer">
       {Object.keys(linkTitles).map((link) => (
-        <NavLink to={link} activeClassName="NavLinkActive">
+        <NavLink to={link} exact activeClassName="NavLinkActive">
           {linkTitles[link]}
         </NavLink>
       ))}
