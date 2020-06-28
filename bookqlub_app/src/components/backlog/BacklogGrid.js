f120c074bbc00c50ec2a81cd4ed78859f0f8893d
Daniel Fujii <danifujii@gmail.com>
2020-06-28 14:55:32 -0300

    Delete UI

--- bookqlub_app/src/components/backlog/BacklogGrid.js
+++ bookqlub_app/src/components/backlog/BacklogGrid.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid } from "@material-ui/core";
 
 import { Review } from "../review_grid/Review";
+import { BacklogDeleteDialog } from "./BacklogDelete";
 
 export const BacklogGrid = (props) => {
   return (
@@ -9,10 +10,37 @@ export const BacklogGrid = (props) => {
       <Grid container spacing={2}>
         {props.backlog.map((backlog, idx) => (
           <Grid item lg={4} md={6} xs={12} key={idx}>
-            <Review book={backlog.book} {...props} />
+            <BacklogEntry book={backlog.book} {...props} />
           </Grid>
         ))}
       </Grid>
+    </div>
+  );
+};
+
+const BacklogEntry = (props) => {
+  const { book, onDelete } = props;
+
+  const [deleteOpen, setDeleteOpen] = useState(false);
+
+  const onDeleteClick = () => {
+    setDeleteOpen(true);
+  };
+
+  const handleDelete = () => {
+    setDeleteOpen(false);
+    onDelete();
+  };
+
+  return (
+    <div>
+      <Review onDeleteClick={onDeleteClick} book={book} />
+      <BacklogDeleteDialog
+        bookId={props.book.id}
+        open={deleteOpen}
+        onClose={() => setDeleteOpen(false)}
+        onDelete={handleDelete}
+      />
     </div>
   );
 };
