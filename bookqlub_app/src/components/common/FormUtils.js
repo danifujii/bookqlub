47ce56b00d81e585174dbf3f22ca34aa3d00e47a
Daniel Fujii <danifujii@gmail.com>
2020-07-05 14:40:52 -0300

    Remove duplicated function

--- bookqlub_app/src/components/common/FormUtils.js
+++ bookqlub_app/src/components/common/FormUtils.js
@@ -18,18 +18,3 @@ export const getFormError = (error) => {
       return "Invalid field value";
   }
 };
-
-export const getFormError = (error) => {
-  if (!error) return undefined;
-
-  switch (error.type) {
-    case "required":
-      return "Field required";
-    case "minLength":
-      return "Field value is too short";
-    case "maxLength":
-      return "Field value is too long";
-    default:
-      return "Invalid field value";
-  }
-};
